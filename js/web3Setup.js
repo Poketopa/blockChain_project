66369e949c18b3ea6f0acd90088c8f7f338c48df
HanSuYeon <js7122420@gmail.com>
2024-09-30 23:18:00 +0900

    create: abi 파일 생성, web3 코드 작성

---
+++ js/web3Setup.js
@@ -0,0 +1,9 @@
+import { abi as contractABI } from '../js/abi/OrigAuthToken.js';
+
+const RPC_URL = 'http://61.32.68.66:80';
+
+export const web3 = new Web3(RPC_URL);
+
+const contractAddress = '0x53f40fd4e98d3e70c14dcc4e3d00e2c59b9b567a';
+
+export const contract = new web3.eth.Contract(contractABI, contractAddress);
