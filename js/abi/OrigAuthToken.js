1ed3d8a1a4b5f3d3ffcabf9688f9224a44005d71
HanSuYeon <js7122420@gmail.com>
2024-10-02 20:02:56 +0900

    update: 컨트랙트 주소, abi 변경

--- js/abi/OrigAuthToken.js
+++ js/abi/OrigAuthToken.js
@@ -505,4 +505,27 @@ export const abi = [
     type: 'function',
     constant: true,
   },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'from',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'nftTransfer',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
 ];
--- js/web3Setup.js
+++ js/web3Setup.js
@@ -4,6 +4,6 @@ const RPC_URL = 'http://61.32.68.66:80';
 
 export const web3 = new Web3(RPC_URL);
 
-export const contractAddress = '0x53f40fd4e98d3e70c14dcc4e3d00e2c59b9b567a';
+export const contractAddress = '0xb69909aA2d32023f56fC69B1893cB839B06B5c36';
 
 export const contract = new web3.eth.Contract(contractABI, contractAddress);
